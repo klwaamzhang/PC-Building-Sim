f72b3cef51c3c7fe6f02a1baf9d55a824eb04376
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2020-04-07 19:27:31 -0400

    OpeningPanel completed

---
+++ Assets/Scripts/OpeningPanelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningPanelController : MonoBehaviour
+{
+    public static bool isOpeningPanelOpened = true;
+    GameObject openingPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        openingPanel = GameObject.Find("UI/Canvas/OpeningPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpeningPanelOpened && Input.GetKeyDown(KeyCode.Return))
+        {
+            isOpeningPanelOpened = false;
+            openingPanel.SetActive(false);
+        }
+    }
+}
