d379d2707e2e691f0123b4a40201603e73a722b9
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2020-04-07 18:54:32 -0400

    Finished the intro panel funtion

---
+++ Assets/Scripts/IntroPanelController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroPanelController : MonoBehaviour
+{
+    public static bool isIntroPanelOpened = false;
+
+    GameObject introPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introPanel = GameObject.Find("UI/Canvas/IntroPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isIntroPanelOpened && Input.GetKeyDown("escape"))
+        {
+            introPanel.SetActive(false);
+        }
+    }
+
+    public void ShowIntroPanel(bool openIntroPanel, Sprite pcItemIntroSprite, string introNameText, Text pcItemIntroContent)
+    {
+        // Show Intro Panel
+        isIntroPanelOpened = openIntroPanel;
+        introPanel.SetActive(true);
+        // Set intro image
+        Image introImage = introPanel.transform.GetChild(0).GetComponent<Image>();
+        introImage.sprite = pcItemIntroSprite;
+        // Set intro heading
+        Text introName = introPanel.transform.GetChild(1).GetComponent<Text>();
+        introName.text = introNameText;
+        // Set intro content
+        Text introContent = introPanel.transform.GetChild(2).GetComponent<Text>();
+        introContent.text = pcItemIntroContent.text;
+    }
+}
--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -6,14 +6,16 @@ using UnityEngine.UI;
 public class ItemManager : MonoBehaviour
 {
     public Sprite pcItemSprite;
+    public Sprite pcItemIntroSprite;
+    public Text pcItemIntroContent;
 
     private static int itemCount = 0;
-    Image slotImage;
+    string introNameText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        introNameText = gameObject.name;
     }
 
     // Update is called once per frame
@@ -24,13 +26,21 @@ public class ItemManager : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
+        gameObject.SetActive(false);
+        SetPCItemToSlot();
+        GameObject gameUI = GameObject.Find("UI");
+        gameUI.GetComponent<IntroPanelController>().ShowIntroPanel(true, pcItemIntroSprite, introNameText, pcItemIntroContent);
+    }
+
+    private void SetPCItemToSlot()
+    {
         itemCount++;
-        slotImage = GameObject.Find("InventoryUI/Canvas/Slots/Slot" + itemCount).GetComponent<Image>();
+        // Set slot image
+        Image slotImage = GameObject.Find("UI/Canvas/Slots/Slot" + itemCount).GetComponent<Image>();
         slotImage.sprite = pcItemSprite;
+        // set slot image alpha to 255
         Color slotImageColor = slotImage.color;
         slotImageColor.a = 255f;
         slotImage.color = slotImageColor;
-        gameObject.SetActive(false);
     }
-
 }
