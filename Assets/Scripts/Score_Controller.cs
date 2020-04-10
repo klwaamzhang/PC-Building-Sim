0b6b76aaf5eb5bb86e9b7f6a7967425d473cf6f9
Nalturan <XxnukeraiderxX@gmail.com>
2020-04-10 19:42:22 -0400

    Andded all of my required parts
    Added Main Menu, Instructions and Scoring System

---
+++ Assets/Scripts/Score_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Controller : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void IncreaseScore(int addScore)
+    {
+            score += addScore;
+            Debug.Log(addScore);
+    }
+
+}
