2b0b2ae22f774acf2f8f1824508ed91ed4ef09d4
Abby <AbbyNode@users.noreply.github.com>
2020-04-11 11:55:36 -0400

    Clean up

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -15,7 +15,7 @@ public class PickupController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemCount = 0;
     }
     
     void Update()
